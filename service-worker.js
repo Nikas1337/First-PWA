c4817dcce1a4e5c39c2ea16f7fd99c9685725206
Nikita <giriio@yandex.ru>
2019-03-28 17:31:19 +0300

    sw good

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,21 @@
 var cacheName = 'weatherPWA-v4';
 var filesToCache = [
     '/index.html',
-    '/styles/ud811.css',
-    '/scripts/app.js',
-    '/scripts/localforage.js',
-    '/images/clear.png',
-    '/images/cloudy-scattered-showers.png',
-    '/images/cloudy.png',
-    '/images/fog.png',
-    '/images/ic_add_white_24px.svg',
-    '/images/ic_refresh_white_24px.svg',
-    '/images/partly-cloudy.png',
-    '/images/rain.png',
-    '/images/scattered-showers.png',
-    '/images/sleet.png',
-    '/images/snow.png',
-    '/images/thunderstorm.png',
-    '/images/wind.png'
+    'scripts/app.js',
+    'scripts/localforage.js',
+    'images/clear.png',
+    'images/cloudy-scattered-showers.png',
+    'images/cloudy.png',
+    'images/fog.png',
+    'images/ic_add_white_24px.svg',
+    'images/ic_refresh_white_24px.svg',
+    'images/partly-cloudy.png',
+    'images/rain.png',
+    'images/scattered-showers.png',
+    'images/sleet.png',
+    'images/snow.png',
+    'images/thunderstorm.png',
+    'images/wind.png'
 ];
 
 self.addEventListener('install', function (e) {
