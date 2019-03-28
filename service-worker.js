2fbd25764ee921fd9f1df9702184048df3381c58
Nikita <giriio@yandex.ru>
2019-03-28 17:44:43 +0300

    all is working

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,21 @@
 var cacheName = 'weatherPWA-v4';
 var filesToCache = [
-    '/First-PWA/index.html'
+    '/First-PWA/index.html',
+    '/First-PWA/scripts/app.js',
+    '/First-PWA/scripts/localforage.js',
+    '/First-PWA/images/clear.png',
+    '/First-PWA/images/cloudy-scattered-showers.png',
+    '/First-PWA/images/cloudy.png',
+    '/First-PWA/images/fog.png',
+    '/First-PWA/images/ic_add_white_24px.svg',
+    '/First-PWA/images/ic_refresh_white_24px.svg',
+    '/First-PWA/images/partly-cloudy.png',
+    '/First-PWA/images/rain.png',
+    '/First-PWA/images/scattered-showers.png',
+    '/First-PWA/images/sleet.png',
+    '/First-PWA/images/snow.png',
+    '/First-PWA/images/thunderstorm.png',
+    '/First-PWA/images/wind.png'
 ];
 
 self.addEventListener('install', function (e) {
