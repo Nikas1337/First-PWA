b243765014e24593494fb85d80b0b633ed047b1a
Nikita <giriio@yandex.ru>
2019-03-28 17:35:46 +0300

    app

--- scripts/app.js
+++ scripts/app.js
@@ -196,7 +196,7 @@
 })();
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-      .register('../service-worker.js')
+      .register('/First-PWA/service-worker.js')
       .then(function() {
         console.log('Service Worker Registered');
       });
